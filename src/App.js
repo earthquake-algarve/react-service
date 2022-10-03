619529ea1e6675d75601a04d291c0726ae3a455c
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2022-10-03 22:41:27 +0100

    feat: setting navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+/* import Services from './pages/Services'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
+import Careers from './pages/Careers'*/
+import Home from './pages/Home' 
+import {Route , Routes} from 'react-router-dom'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/*<Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/careers' element={<Careers />} />
+          <Route path='/contact' element={<Contacts />} /> */}
+        </Routes>
+
+      </div>
+    </>
+  )
 }
 
-export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom'
+import { MagnifyingGlass, Heart, ShoppingCart, Login, Menu } from '../Icons'
+
+
+
+
+export default function Navbar() {
+
+    return (
+        <>
+            <nav className="topnav">
+                <Link to="/" className="logo">
+                    LOGO
+                </Link>
+                <ul>
+                    <Link to="/"><MagnifyingGlass /></Link>
+                    <Link to='/favorites'><Heart /></Link>
+                    <Link to='/shoppingcart'><ShoppingCart /></Link>
+                    <Link to='/login'><Login /></Link>
+                    <Link to="/menu"><Menu /></Link>
+                </ul>
+            </nav>
+        </>
+    )
+}
