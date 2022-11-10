e8eaca34d65886dd332fab02bc27ba5da9f223cd
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2022-11-10 14:54:27 +0000

    cleaning: shopping cart page

--- src/pages/shoppingCart/ShoppingCart.js
+++ src/pages/shoppingCart/ShoppingCart.js
@@ -1,143 +1,139 @@
 import React from 'react'
 import './shoppingCart.css'
-import { Heart,BinIcon } from '../../Icons';
+import { Heart,BinIcon, ArrowDown } from '../../Icons';
 
 
 export default function ShoppingCart() {
 
     return (
         <>
-            <div className="pgina-de-shoppingcart">
-                <div className="resumo-top-div">
-                    <div className="shopping-cart-div">
-                        <p className="summary-p">Shopping Cart</p>
+            <div className="page-shoppingcart">
+
+                <div className="resumo-top">
+                    <div className="page-title">
+                        <span className="span">Shopping Cart</span>
                     </div>
 
-                    <div className="items-60">
+                    <div className="items-price">
                         <span className='span'>2 items</span>
                         <span className="span"> | 60€</span>
                     </div>
                 </div> 
+
+                <div className="item1">
                 
+                    <div className="img1">
+                        <img alt="" className='image'
+                        src="https://contes-toten-api.herokuapp.com/passeio/get/2"
+                        />
+                    </div>
 
+                    <div className="conteudo-item1">
+                        <div className='tour-price'>
+                            <span className="tour">Zoo de Lagos</span>
+                            <span className="price">30€</span>
+                        </div>
 
-                <div className="item1-div">
-                    <div className="conteudo-item1-div">
+                        <div className="date">
+                            <span>Data: </span>
+                            <span>31/11/2022</span>                
+                        </div>
+
+                        <div className="quantidade">
+                            Quantidade: 
+
+                            <select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>9</option>
+                                <option>10</option>
+                            </select>
+                        </div>
+
                         <div className="icons-heart-bin">
                             <Heart />
                             <BinIcon />
                         </div>
 
-                        <div className="img-item2-div">
-                            <img
-                            className="slidesplash-755x470-icon"
-                            alt=""
-                            src="https://contes-toten-api.herokuapp.com/passeio/get/2"
-                            />
-                        </div>
-
-                        <b className="slide-splash-30">Zoo de Lagos 30€</b>
-
-                        <div className="data-30112022-div">Data: 31/11/2022</div>
-
-                        <div className="quantidade-div">
-                            <div className="quantidade-1-div">Quantidade: 
-
-                                <select>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                    <option>6</option>
-                                    <option>7</option>
-                                    <option>8</option>
-                                    <option>9</option>
-                                    <option>10</option>
-                                </select>
-                            </div>
-                        </div>
-
                     </div>
 
                 </div>
+                
+                <div className="item1">
+                
+                    <div className="img1">
+                        <img alt="" className='image'
+                        src="https://contes-toten-api.herokuapp.com/passeio/get/4"
+                        />
+                    </div>
 
-                <div className="item2-div">
-                    <div className="conteudo-item2-div">
-                        <div className="icons">
+                    <div className="conteudo-item1">
+                        <div className='tour-price'>
+                            <span className="tour">Slide Splash</span>
+                            <span className="price">30€</span>
+                        </div>
+
+                        <div className="date">
+                            <span>Data: </span>
+                            <span>31/11/2022</span>                
+                        </div>
+
+                        <div className="quantidade">
+                            Quantidade: 
+
+                            <select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                                <option>6</option>
+                                <option>7</option>
+                                <option>8</option>
+                                <option>9</option>
+                                <option>10</option>
+                            </select>
+                        </div>
+
+                        <div className="icons-heart-bin">
                             <Heart />
                             <BinIcon />
                         </div>
 
-                        <div className="img-item2-div">
-                            <img
-                            className="slidesplash-755x470-icon"
-                            alt=""
-                            src="https://contes-toten-api.herokuapp.com/passeio/get/4"
-                            />
-                        </div>
-
-                        <b className="slide-splash-30">Slide Splash 30€</b>
-
-                        <div className="data-30112022-div">Data: 30/11/2022</div>
-
-                        <div className="quantidade-div">
-                            <div className="quantidade-1-div">Quantidade: 
-                            {/* <ArrowDown /> */}
-                            <select>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                    <option>6</option>
-                                    <option>7</option>
-                                    <option>8</option>
-                                    <option>9</option>
-                                    <option>10</option>
-                                </select>
-                            </div>
-                        </div>
-
                     </div>
 
                 </div>
 
+
                 <div className="summary-div">
-                    <b className="summary-b">
-                        <p className="summary-p">Summary</p>
-                    </b>
-                    <div className="conteudo-summary-div">
-                        <div className="promo-code-div">
-                            <div className="do-you-have-a-promo-code">
+                    <div className="summary-title">Summary</div>
+
+                    <div className="conteudo-summary">
+                        <div className="promo-code">
                             Do you have a promo code?
-                            </div>
-                            <img
-                            className="dashiconsarrow-down-alt2"
-                            alt=""
-                            src="../dashiconsarrowdownalt2.svg"
-                            />
+                            <ArrowDown/>
                         </div>
 
-                        <div className="subtotal-div">
-                            <div className="subtotal-div1">
-                            <span className="subtotal-span">Subtotal</span>
-                            <b>{`: `}</b>
-                            </div>
-                            <div className="div">60€</div>
+                        <div className="subtotal">
+                            <span>Subtotal:</span>
+                            <span >60€</span>
                         </div>
 
-                        <div className="taxes-div">
-                            <div className="total-div1">Taxes:</div>
-                            <div className="div">0€</div>
+                        <div className="taxes">
+                            <span>Taxes:</span>
+                            <span>0€</span>
                         </div>
 
-                        <div className="total-div">
-                            <div className="total-div1">{`Total: `}</div>
-                            <div className="div">60€</div>
+                        <div className="total">
+                            <span>Total:</span>
+                            <span>60€</span>
                         </div>
-
-
 
                         <div className="redirect-div">
                             <div className='continuar-compra'>
