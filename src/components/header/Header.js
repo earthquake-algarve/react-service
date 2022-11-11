dfb2800e1a00356272587d4bde4e2e197e2bcf88
luiz <luiz.contes@hotmail.com>
2022-11-11 01:11:01 +0000

    feat: search bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,48 +10,45 @@ import './header.css'
 
 export default function Header() {
     return (
-      <>
-          <Container fluid className='container'>
-              {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
-                  <Navbar key={expand}  expand={expand} className="navbar-custom" variant='light' fixed='top'>
-                      <Link to="/" id='logo'>LOGO </Link>
+        <>
+            <Container fluid className='container'>
+                {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
+                    <Navbar key={expand} expand={expand} className="navbar-custom" variant='light' fixed='top'>
+                        <Link to="/" id='logo'>LOGO </Link>
 
-                      <div className='search-box'>
-                        <form>
-                            <input type="text" class="search-click"/>
+                        <div className='search'>
                             <Link className='search-btn' id='search'><MagnifyingGlass /></Link>
+                            <input type="text" class="search-input" />
 
-                        </form>
-                      </div>
+                            <Link to="/shoppingcart" class='shoppingcart' id='shoppingcart'><ShoppingCart /></Link>
+                            <Link to="/checkout" id='checkout'><CheckoutIcon /></Link>
+                            <Link to="/login" id='login'><Login /></Link>
+                        </div>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    LOGO
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Link to="/news" id='news'>News</Link>
+                                    <Link to="/about" id='about'>About</Link>
+                                    <Link to="/contact" id='contact'>Contact</Link>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
 
-                      <Link to="/shoppingcart" id='shoppingcart'><ShoppingCart /></Link>
-                      <Link to="/checkout" id='checkout'><CheckoutIcon /></Link>
-                      <Link to="/login" id='login'><Login /></Link>
-                      <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                      <Navbar.Offcanvas
-                      id={`offcanvasNavbar-expand-${expand}`}
-                      aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                      placement="end"
-                      >
-                          <Offcanvas.Header closeButton>
-                              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                              LOGO
-                              </Offcanvas.Title>
-                          </Offcanvas.Header>
-                          <Offcanvas.Body>
-                              <Nav className="justify-content-end flex-grow-1 pe-3">
-                                  <Link to="/news" id='news'>News</Link>
-                                  <Link to="/about" id='about'>About</Link>
-                                  <Link to="/contact" id='contact'>Contact</Link>
-                              </Nav>
-                          </Offcanvas.Body>
-                      </Navbar.Offcanvas>
-          
-                  </Navbar>
-        ))}
-  
-          </Container>
-              
-      </>
+                    </Navbar>
+                ))}
+
+            </Container>
+
+        </>
     );
-  }
+}
