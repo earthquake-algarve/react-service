a233eec7de99da1ccbb6c3066a8333a95b08441d
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2022-11-14 23:42:42 +0000

    cleaning details page

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -4,58 +4,44 @@ import { ArrowLeft, Calendar } from '../../Icons';
 
 const EventoDetalhes = () => {
   return (
-    <div className="evento-detalhes-div">
+    <div className="details-page">
         
-        <a className="voltar-b" href='/'><ArrowLeft/>Voltar</a>
+        <a className="back" href='/'><ArrowLeft/></a>
 
-        <div className="card-detalhes-div">
-            <div className="card-detalhes-div1">
-                <div className="img-div">
-                <img className="benagil-2-icon" alt="" src="https://contes-toten-api.herokuapp.com/passeio/get/1" />
-                </div>
-                <div className="detalhes-div">
-                    <div className="titulo-div">
-                    <b className="ttulo-do-passeio">Título do passeio</b>
-                    </div>
-                    
-                    <div className="preo-div">
-                    <b className="b">€</b>
-                    </div>
-                    
-                    <div className="descrio-div">
-                    <b className="descrio-b">Descrição</b>
-                    </div>
-
-                    <div className="durao-div">
-                    <b className="durao-1h-25min">Duração: 1h 25min</b>
-                    </div>
-
-                    <div className="localidade-div">
-                    <b className="localidade-b">Localidade</b>
-                    </div>
-
-                    <div className="data-div">
-
-                        <div className="data-02112022-b">Data: 02/11/2022
-                        </div>
-                        <div className="calendario"><Calendar /></div>
-                        
-                    </div>
-
-                    <div className="horrio-div">
-                        <b className="horrio-1030-b">Horário: 10:30</b>
-                    </div>
-
-                </div>
-                <div className="button-adicionar-div1">
-                    <a  href="/shoppingcart" className="your-text-in-here">Adicionar ao carrinho</a>
-                </div>
+        <div className="container">
+            <div className="img-div">
+                <img className="benagil-img" alt="benagil img" src="https://contes-toten-api.herokuapp.com/passeio/get/1" />
             </div>
 
+            <div className="details">
+                <div className='title-price-div'>
+                    <div className="title">Título do passeio</div>
+                    <div className="price">€</div>
+                </div>
 
+                
+                <div className="description">Description</div>
 
+                <div className="duration">Duração: 1h 25min</div>
+
+                <div className="localidade">Localidade</div>
+
+                <div className="date-div">
+
+                    Data: 02/11/2022
+                    <button className="calendar"><Calendar /></button>
+                    
+                </div>
+
+                <div className="horario">Horário: 10:30</div>
+
+            </div>
+
+            <div className="button-adicionar">
+                <a  href="/shoppingcart" className="add-to-cart">Adicionar ao carrinho</a>
+            </div>
         </div>
-      
+
     </div>
   );
 };
