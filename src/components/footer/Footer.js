d276b67d7a60c123260eb190bb01e5540f5f8971
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2022-11-18 22:33:37 +0000

    feat: finished footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,62 +2,74 @@ import './footer.css'
 
 const Footer = () => {
     return (
-        <div className="footer-div">
-        <div className="frame-div" />
-        <div className="frame-div1">
-          <div className="selecione-um-pais">Selecione um pais</div>
-          <div className="frame-div2">
-            <img
-              className="icoutline-greater-than-icon"
-              alt=""
-              src="img/icon-greater-than.svg"
-            />
-            <img
-              className="twemojiflag-portugal-icon"
-              alt=""
-              src="/img/icon-flag-portugal.svg"
-            />
-            <div className="portugal-div">Portugal</div>
-          </div>
+        <div className="footer">
+
+            <div className="footer-links">
+                <span><b>Informações</b></span>
+                <a href="/" className="link">Contactos</a>
+                <a href="/" className="link">Condicoes legais</a>
+                <a href="/" className="link">Politica de privacidade</a>
+                <a href="/" className="link">Politica de cookies</a>
+                <a href="/" className="link">Gerir preferencias</a>
+                <a href="/" className="link">Registo empresas</a>
+                <a href="/" className="link">Quem somos?</a>
+                <a href="/" className="link">Careers</a>
+            </div>
+
+            <div className="social-media">
+                <span><b>Siga-nos em</b></span>
+
+                <div className="social-icons">
+                    <a href="/">
+                        <img
+                        className="icon"
+                        alt=""
+                        src="/img/icons-facebook.svg"
+                        />
+                    </a>
+
+                    <a href="/">
+                        <img
+                            className="icon"
+                            alt=""
+                            src="/img/icons-twitter.svg"
+                        />
+                    </a>
+
+                    <a href="/">
+                        <img
+                            className="icon"
+                            alt=""
+                            src="/img/icons-pinterest.svg"
+                        />
+                    </a>
+
+                    <a href="/">
+                        <img
+                            className="icon"
+                            alt=""
+                            src="/img/icons-instagram.svg"
+                        />
+                    </a>
+                </div>
+            </div>
+
+
+
+            <div className="language">
+                <span><b>Selecione um país</b></span>
+            
+                <div className="input-pais">
+                    <img
+                    className="icon"
+                    alt=""
+                    src="/img/icon-flag-portugal.svg"
+                    />
+                    <input type="text" />
+                </div>
+            </div>
+
         </div>
-        <div className="contactar-condicoes-legais-pol-div">
-          <span className="contactar-condicoes-legais-pol-span">
-            <p className="contactar-p">Contactar</p>
-            <p className="contactar-p">Condicoes legais</p>
-            <p className="contactar-p">Politica de privacidade</p>
-            <p className="contactar-p">Politica de cookies</p>
-            <p className="contactar-p">Gerir preferencias</p>
-            <p className="contactar-p">Registo empresas</p>
-            <p className="contactar-p">Quem somos?</p>
-            <p className="contactar-p">Equipa Editorial</p>
-            <p className="careers-p">Careers</p>
-          </span>
-        </div>
-        <div className="informacoes-div">Informações</div>
-        <div className="siga-nos-em-div">Siga-nos em</div>
-        <div className="social-icons-div">
-          <img
-            className="akar-iconsfacebook-fill"
-            alt=""
-            src="/img/icons-facebook.svg"
-          />
-          <img
-            className="akar-iconsfacebook-fill"
-            alt=""
-            src="/img/icons-twitter.svg"
-          />
-          <img
-            className="akar-iconsfacebook-fill"
-            alt=""
-            src="/img/icons-pinterest.svg"
-          />
-          <img
-            className="akar-iconsfacebook-fill"
-            alt=""
-            src="/img/icons-instagram.svg"
-          />
-        </div>
-      </div>
     )
 } 
 
