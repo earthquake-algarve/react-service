6df9adaf3ec666160465a58f621c62530acaaa81
luiz <luiz.contes@hotmail.com>
2022-12-23 14:54:54 +0000

    refactor: fetching and rendering tours

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,48 +3,31 @@ import MainCard from '../components/mainCard/MainCard'
 import Slider from '../components/slider/Slider'
 import { useState, useEffect } from 'react'
 
-
 export default function Home() {
 
     const [passeios, setPasseios] = useState([])
     const [mainCard, setMainCard] = useState({})
 
-
     useEffect(() => {
-        showTours()
-    }, [])
-
-    async function showTours() {
         fetch('http://entertours-ofertas.us-east-1.elasticbeanstalk.com/get')
             .then(response => response.json())
-            .then(data => {
-                setPasseios(data)
-                showMainCard(data)
+            .then(passeios => {
+                setPasseios(passeios)
+                let qtdPasseios = passeios.length
+                let passeioIndex = Math.floor(Math.random() * (qtdPasseios - 1))
+                setMainCard(passeios[passeioIndex])
             })
-    }
+    }, [])
 
-    function showMainCard(data){
-        let x = data.length
-
-        let y = Math.floor(Math.random()*(x-1))
-
-        setMainCard(data[y])
-    }
-
-    function handlePasseio(e){
+    function handlePasseio(e) {
         e.preventDefault()
-
-        passeios.map(p => {
-            if(e.target.id === p.id){
-                setMainCard(p)
-            }
-        })
+        setMainCard(() => passeios.find( p => e.target.id === p.id && p ))
     }
 
     return (
         <>
-            <MainCard mainCard={mainCard}/>
-            <Slider passeios={passeios} handlePasseio={handlePasseio}/>
+            <MainCard mainCard={mainCard} />
+            <Slider passeios={passeios} handlePasseio={handlePasseio} />
         </>
     )
 }
